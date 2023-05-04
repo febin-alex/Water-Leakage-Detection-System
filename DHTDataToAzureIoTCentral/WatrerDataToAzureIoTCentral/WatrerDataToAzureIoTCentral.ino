0ba746b0ec0dfc2159f46a6dd331348199e75128
febin-alex <68898669+febin-alex@users.noreply.github.com>
2023-05-04 09:14:12 +0530

    added nodemcu code

---
+++ DHTDataToAzureIoTCentral/WatrerDataToAzureIoTCentral/WatrerDataToAzureIoTCentral.ino
@@ -0,0 +1,120 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full
+// license information.
+
+#include <ESP8266WiFi.h>
+#include "src/iotc/common/string_buffer.h"
+#include "src/iotc/iotc.h"
+
+
+#include <SoftwareSerial.h>
+#include <ArduinoJson.h>
+
+
+#define WIFI_SSID "realme X2"
+#define WIFI_PASSWORD "123456qwerty"
+
+const char* SCOPE_ID = "0ne009F7367";
+const char* DEVICE_ID = "devid";
+const char* DEVICE_KEY = "BCt8ONlaqlLHikpAHpMO9uMvrjaiHAcWLA1VRY2o1HI=";
+SoftwareSerial nodemcu(D6, D5);
+
+void on_event(IOTContext ctx, IOTCallbackInfo* callbackInfo);
+#include "src/connection.h"
+
+void on_event(IOTContext ctx, IOTCallbackInfo* callbackInfo) {
+  // ConnectionStatus
+  if (strcmp(callbackInfo->eventName, "ConnectionStatus") == 0) {
+    LOG_VERBOSE("Is connected ? %s (%d)",
+                callbackInfo->statusCode == IOTC_CONNECTION_OK ? "YES" : "NO",
+                callbackInfo->statusCode);
+    isConnected = callbackInfo->statusCode == IOTC_CONNECTION_OK;
+    return;
+  }
+
+  // payload buffer doesn't have a null ending.
+  // add null ending in another buffer before print
+  AzureIOT::StringBuffer buffer;
+  if (callbackInfo->payloadLength > 0) {
+    buffer.initialize(callbackInfo->payload, callbackInfo->payloadLength);
+  }
+
+  LOG_VERBOSE("- [%s] event was received. Payload => %s\n",
+              callbackInfo->eventName, buffer.getLength() ? *buffer : "EMPTY");
+
+  if (strcmp(callbackInfo->eventName, "Command") == 0) {
+    LOG_VERBOSE("- Command name was => %s\r\n", callbackInfo->tag);
+  }
+}
+
+void setup() {
+  Serial.begin(9600);
+  nodemcu.begin(9600);
+  connect_wifi(WIFI_SSID, WIFI_PASSWORD);
+  connect_client(SCOPE_ID, DEVICE_ID, DEVICE_KEY);
+
+  if (context != NULL) {
+    lastTick = 0;  // set timer in the past to enable first telemetry a.s.a.p
+  }
+   while (!Serial) continue;
+}
+
+void loop() {
+
+  StaticJsonBuffer<1000> jsonBuffer;
+  JsonObject& data = jsonBuffer.parseObject(nodemcu);
+
+  if (data == JsonObject::invalid()) {
+    //Serial.println("Invalid Json Object");
+    jsonBuffer.clear();
+    return;
+  }
+
+  Serial.println("JSON Object Recieved");
+  Serial.print("Recieved flow:  ");
+  float flow = data["flow"];
+  Serial.println(flow);
+  Serial.print("Recieved material:  ");
+  float material = data["material"];
+  Serial.println(material);
+
+  Serial.println("-----------------------------------------");
+
+
+  
+  if (isConnected) {
+
+    unsigned long ms = millis();
+    if (ms - lastTick > 10000) {  // send telemetry every 10 seconds
+      char msg[64] = {0};
+      int pos = 0, errorCode = 0;
+
+      lastTick = ms;
+      if (loopId++ % 2 == 0) {  // send telemetry
+        pos = snprintf(msg, sizeof(msg) - 1, "{\"flow\": %f}",
+                       flow);
+        errorCode = iotc_send_telemetry(context, msg, pos);
+        
+        pos = snprintf(msg, sizeof(msg) - 1, "{\"material\":%f}",
+                       material);
+        errorCode = iotc_send_telemetry(context, msg, pos);
+          
+      } else {  // send property
+        
+      } 
+  
+      msg[pos] = 0;
+
+      if (errorCode != 0) {
+        LOG_ERROR("Sending message has failed with error code %d", errorCode);
+      }
+    }
+
+    iotc_do_work(context);  // do background work for iotc
+  } else {
+    iotc_free_context(context);
+    context = NULL;
+    connect_client(SCOPE_ID, DEVICE_ID, DEVICE_KEY);
+  }
+
+}
